68aaa65b4605ed3f95b1e30f491e738c159c98f6
Samarth Dua <samarthdua@gmail.com>
2018-10-16 15:59:27 -0400

    comment front end pt2

--- Public/js/comment.app.js
+++ Public/js/comment.app.js
@@ -1,14 +1,26 @@
-var randomUser = new Vue({
-  el: "#user",
+var commentApp = new Vue({
+  el: "#commentPage",
   data:{
     comment:{
       "comment_id":'',
       "comment_body":''
-    }
+    },
+    commentArray: [],
+    commentForm: {}
   },
   computed: {},
   methods: {
 
+
+    fetchComments() {
+      fetch('api/comment.php')
+      .then( response => response.json() )
+      .then( json => {commentApp.commentArray = json} )
+      .catch( err => {
+        console.log('TEAM LIST ERROR:');
+        console.log(err);
+      })
+    }
   },
   created() {
     this.fetchProfile();
